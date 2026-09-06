92e2d2f9033f41ce3d71c0398bf248761c87fbce
ABN HALLOMAH <alhamawymohamed@gmail.com>
2026-09-06 16:34:58 +0300

    update 19

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { SmoothCursor } from "@/components/ui/smooth-cursor";
-import Script from "next/script";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
@@ -67,20 +66,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           name="google-site-verification"
           content="X-gvcvXbikIWv4dfaHxkPzEaJy-NiDn5GZfDdQdO66I"
         />
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GA_MEASUREMENT_ID}');
-          `}
-        </Script>
       </head>
-     
       <body className="min-h-full flex flex-col light-bg">
         <SmoothCursor />
         {children}
