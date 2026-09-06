ac49f913bdb258aeb44d027333ba00e07bebc51a
ABN HALLOMAH <alhamawymohamed@gmail.com>
2026-09-06 12:21:42 +0300

    upload 15

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   Contact,
   Footer,
 } from "@/components/myComponents/index";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
         <Contact />
         <Footer />
       </main>
+      <Analytics  />
     </>
   );
 }
